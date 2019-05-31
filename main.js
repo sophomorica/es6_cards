2ee4e3060ccfb99f5c48d009ac89d3999c86bb1a
Patrick Lamoureux <patrick@blackcedar.com>
2019-05-31 17:52:37 -0600

    submit button for the array questions

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
 $(document).ready(function(){
   const card = $(".card")
-  const newCard =$(".newCard")
+  const nextCard =$(".nextCard")
   const cardFront = $("#front")
   const cardBack = $("#back")
+  const newQuestion = $("#new_question")
+  const newAnswer = $("#new_answer")
+  const submit = $("#submit")
 
-  // const newCardFunction = () => {
+  submit.on("click", function(){
+    questions.push({
+      "query":`${newQuestion.val()}`,
+      "answer":`${newAnswer.val()}`
+    })
+
+  })
+  var x = 0
+  // const nextCardFunction = () => {
     
   //  cardFront.append("<h1>you did it</h1>")
   // }
-  // function newCardFunc(){
+  // function nextCardFunc(){
     
   // }
-  const question = ["the absolute value of pie", "my name"]
-  newCard.on("click",function(){
-    cardFront.append(`What is ${question[0]}`)
+  const questions = [{
+    query: "what is pie",
+    answer: "pie is awesome"
+  },
+  {
+    query: "what is dirt",
+    answer: "dirt is not awesome"
+  }]
+  nextCard.on("click",function(){
+    // questions.forEach((question)=>{
+    //   cardFront.append(`${question.query}`)
+    //   cardBack.append(`${question.answer}`)
 
+    // })
+  
+
+   if (x === questions.length ){
+     x = 0
+   }
+   cardFront.text(`${questions[x].query}`)
+   cardBack.text(`${questions[x].answer}`)
+   x++
   })
   
  card.on("click",function(){
